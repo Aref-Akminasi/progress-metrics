86612538e91dac329f8b9d012751b51fabeed00e
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-06-25 09:28:47 +0200

    updated content

--- script.js
+++ script.js
@@ -7,25 +7,31 @@ let currentIdx = 0;
 stages.forEach((stage, idx) => {
   stage.addEventListener('click', () => {
     currentIdx = idx;
-    update();
+    //After a stage is clicked the current stage var is updated and based on that stage the progress bar, stages border and stages content should be updated as well
+    updateProgress();
+    updateContent();
   });
 });
 
-function update() {
+function updateProgress() {
   //Updating the progress bar
-  progressBar.style.width = (currentIdx / (stages.length - 1)) * 100 + '%';
+  progressBar.style.width = (currentIdx / (stages.length - 1)) * 100 + '%'; //Here should stages.length decremented by 1, because the steps between the 5 stages are 4.
   //Updating the stages border
   stages.forEach((stage, idx) => {
     idx <= currentIdx
       ? (stage.style.border = 'none')
       : (stage.style.border = 'solid' + borderColor);
   });
+}
+
+function updateContent() {
   //Updating the stage content
   stages.forEach((stage, idx) => {
-    document.querySelector('.stage' + (idx + 1) + '-content').style.display =
+    document.querySelector('.stage' + (idx + 1) + '-content').style.display = //First, display:none; for all stages
       'none';
   });
   document.querySelector(
+    //Seconnd, display that particular stage (the stage that is clicked on)
     '.stage' + (currentIdx + 1) + '-content'
   ).style.display = 'flex';
 }
